05b659928fe713dc61d3cae50d76a96de42da4ba
paraspipre <paras11917@gmail.com>
2024-11-24 23:34:14 +0530

    env

---
+++ src/app/api/route.ts
@@ -0,0 +1,10 @@
+export const dynamic = 'force-static'
+
+import { Client } from "twitter-api-sdk";
+export async function GET() {
+   const client = new Client(process.env.NEXT_PUBLIC_TOKEN as string);
+   const response = await client.tweets.usersIdTweets("elonmusk");
+   console.log("response", response);
+
+   return Response.json({ response })
+}
