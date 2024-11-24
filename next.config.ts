d872c5e9e2d8303229922123e98d4b5b6325bd53
paraspipre <paras11917@gmail.com>
2024-11-24 22:55:50 +0530

    env

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NEXT_PUBLIC_TOKEN: "AAAAAAAAAAAAAAAAAAAAAMWfxAEAAAAAbXebC1sXjBLa82oSCGzlMBn7oNY%3DwwKOmhthW1wv35ukrDAeKfLiuzT6UguynmRhUoezxknK3zEe8a"
-  }
-  
 };
 
 export default nextConfig;
